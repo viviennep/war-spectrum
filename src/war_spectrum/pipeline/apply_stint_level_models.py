47db1cb49a10850657b806fca7cebd1196e5d502
vivienne <viviennepelletier@gmail.com>
2025-10-03 14:11:27 -0700

    rerun

--- src/war_spectrum/pipeline/apply_stint_level_models.py
+++ src/war_spectrum/pipeline/apply_stint_level_models.py
@@ -71,7 +71,7 @@ def attach_xbaseruns(stints, b_terms=b_terms):
         'cs'  : 'xcs',
         'ra'  : 'runs_allowed',
     }
-    return estimate_baseruns(stints, b_terms, name='xbaseruns')
+    return estimate_baseruns(stints, b_terms, name='xbaseruns', cutoff=1000)
 
 def attach_xbaseruns_stuff(stints, b_terms=b_terms):
     b_terms = {
